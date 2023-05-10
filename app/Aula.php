870b6ba75e63252f2fd33c35d8107e4724bf12f7
Lucas Wesley <lucaswesley504@gmail.com>
2023-05-10 08:59:56 -0300

    filtrando aulas

--- app/Aula.php
+++ app/Aula.php
@@ -3,7 +3,14 @@
 namespace app;
 
 class Aula {
-    public function listarAulas() {
-        return ["a", "b"];
+  public function listarAulas() {
+    $items = scandir("aulas");
+    $aulas = [];
+    foreach ($items as $key => $item) {
+      if(is_dir("aulas/".$item) && $item != "." && $item != "..") {
+        $aulas[] = $item;
+      }
     }
+    return $aulas;
+  }
 }
