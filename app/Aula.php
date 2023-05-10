d0acdd56c0b382764c24604f17d30df65c7a959f
Lucas Wesley <lucaswesley504@gmail.com>
2023-05-10 08:46:54 -0300

    Listando aulas

---
+++ app/Aula.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace app;
+
+class Aula {
+    public function listarAulas() {
+        return ["a", "b"];
+    }
+}
--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use app\Aula;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $aula = new Aula();
+    $aulas = $aula->listarAulas();
+
+    $response->getBody()->write(json_encode($aulas));
+    return $response->withHeader("Content-Type", 'application/json');
 });
-$app->setBasePath('/aula');
+$app->setBasePath('/ams');
 $app->run();
